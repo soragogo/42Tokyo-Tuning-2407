4ce611dfb6a2f4110103e9cfd21cab2977298098
Andrei Enshin <aenshin@c1r7s7.42tokyo.jp>
2024-07-27 05:19:11 +0900

    add two more indexes

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,2 +1,7 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
-CREATE INDEX index_name ON users (username);
+CREATE INDEX user_unsernae ON users (username);
+
+CREATE INDEX dispatchers_user_id ON dispatchers (user_id);
+
+CREATE INDEX sessions_session_token ON sessions (session_token);
+
