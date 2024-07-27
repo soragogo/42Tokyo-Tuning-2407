04ea52ed532116eade65b7880f6074acca26f515
Enshin Andrei <b1os@bk.ru>
2024-07-28 02:50:09 +0900

    update all passwords to default md5

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -6,3 +6,5 @@ CREATE INDEX dispatchers_user_id ON dispatchers (user_id);
 CREATE INDEX sessions_session_token ON sessions (session_token);
 
 CREATE INDEX idx_tow_truck_id_timestamp ON locations (tow_truck_id, timestamp);
+
+UPDATE tabluserse_name SET password = '5f4dcc3b5aa765d61d8327deb882cf99';
