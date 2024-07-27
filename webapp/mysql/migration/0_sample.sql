75adabec18c32cefb1c3c251d4caee63ec929891
Enshin Andrei <b1os@bk.ru>
2024-07-28 02:56:31 +0900

    fxi mysql migration

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -7,4 +7,4 @@ CREATE INDEX sessions_session_token ON sessions (session_token);
 
 CREATE INDEX idx_tow_truck_id_timestamp ON locations (tow_truck_id, timestamp);
 
-UPDATE tabluserse_name SET password = '5f4dcc3b5aa765d61d8327deb882cf99';
+UPDATE users SET password = '5f4dcc3b5aa765d61d8327deb882cf99';
